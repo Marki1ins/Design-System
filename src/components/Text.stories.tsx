e27113c6b1de0181c627367873324741aa1f4a5b
Marcos Vinícius <marcosdfcarvalhoo@gmail.com>
2022-10-27 17:38:31 -0300

    Design System

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,54 @@
+import { Text, TextProps } from './Text';
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Lorem ipsum.',
+        size: 'md'
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
+    }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Testando com o &lt;p&gt;parágrafo&lt;&frasl;p&gt;</p>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true
+            }
+        },
+        asChild: {
+            table: {
+                disable: true
+            }
+        }
+    }
+}
